3d8cc76d8a5c321ba2bdd3a9f79fe82146892201
Vinícius Oliveira Gonçalves <viniciusoligon@outlook.com>
2021-08-04 18:11:05 -0300

    comite inicial

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const lista = [
+    {
+        id: 1,
+        nome: "Os Goonies",
+        id: 2,
+        nome: "Pateta",
+    },
+];
+
+app.get("/", function (req, res) {
+    res.send("Teste");
+});
+
+app.get("/filmes", function (req, res) {
+    res.send(lista.filter(Boolean));
+});
+
+
+app.post("/filmes", (req, res) => {
+    const filme = req.body.nome;
+
+    lista.push(filme);
+
+    res.status(201).send("Filme inserido com sucesso!");
+});
+
+app.put("/filmes/:id", (req, res) => {
+    const id = +req.params.id;
+
+    const novoFilme = req.body.nome;
+
+    lista[id] = novoFilme;
+
+    res.send("Filme atualizado com sucesso!");
+});
+
+app.delete("/filmes/:id", (req, res) => {
+    const id = +req.params.id;
+
+    delete lista[id];
+
+    res.send("Filme excluído com sucesso!");
+});
+
+app.listen(3000, () => {
+    console.log("Aplicação rodando em http://localhost:3000/");
+});
